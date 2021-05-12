985704c37549a5e776a1c5d12e0aa1e1fc561ad6
Josh Brito <jbrito6492@gmail.com>
2021-05-11 20:12:07 -0700

    added routes and fixed import statement

--- server/app.py
+++ server/app.py
@@ -1,3 +1,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'Hello World'
+
+@app.route('/schedule')
+def schedule():
+    return 'schedule'
+
+@app.route('/upload', methods=['POST'])
+def upload_image(image):
+    pass
