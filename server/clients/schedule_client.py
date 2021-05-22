b5c22ffecde010e843b44ec5c86cf84ab0f049bf
Josh Brito <jbrito6492@gmail.com>
2021-05-21 23:50:11 -0700

    cleaned up soup

--- server/clients/schedule_client.py
+++ server/clients/schedule_client.py
@@ -18,19 +18,25 @@ class ScheduleClient:
 
     def get_url(self, path=None):
         return f"{self.domain}/{path}" if path else self.domain
-    
+
     def parse_soup(self, soup):
-        tables = soup.find_all('table', class_ = "schedule")
+        tables = soup.find_all('table', class_="schedule")
         for table in tables:
             rows = soup.find_all('tr')
             row_item = []
             for row in rows:
                 data_items = row.find_all('td')
+
                 for data_item in data_items:
                     if 'tickets' in data_item.get('class', {}):
                         continue
-                    data_item = data_item.get('data-date') if data_item.text.strip() == '' else data_item.text
-                    if data_item:
-                        row_item.append(data_item)
+                    elif not data_item.text.strip():
+                        date = data_item.get('data-date')
+                        if date:
+                            row_item.append(date)
+                    elif data_item:
+                        row_item.append(data_item.text)
+
                 if len(row_item):
                     self.one_week_schedule.append(row_item)
+                row_item = []
